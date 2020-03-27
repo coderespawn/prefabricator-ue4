9201431738d23ac0f043064107f26b28b1ea5d86
Ali Akbar <ali.akbar@gmail.com>
2020-03-27 12:26:28 +0530

    Updated unity build directive in the construction system build.cs modules

--- Source/ConstructionSystemEditor/ConstructionSystemEditor.Build.cs
+++ Source/ConstructionSystemEditor/ConstructionSystemEditor.Build.cs
@@ -6,7 +6,7 @@ namespace UnrealBuildTool.Rules
 	{
 		public ConstructionSystemEditor(ReadOnlyTargetRules Target) : base(Target)
         {
-            bFasterWithoutUnity = true;
+            bUseUnity = false;
             PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
             PublicIncludePaths.AddRange(
 				new string[] {
