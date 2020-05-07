b1d532ddcebc36e44099a0001dd79eaea3c67d05
Ali Akbar <ali.akbar@gmail.com>
2020-05-07 08:10:39 +0530

    Revert "Implemented correct property field casting. This fixes the crash issue when saving/loading prefabs"
    This reverts commit 5f428c7326978d9fb704e3104d0295964d6a8c17.

--- Source/PrefabricatorRuntime/Private/Prefab/PrefabTools.cpp
+++ Source/PrefabricatorRuntime/Private/Prefab/PrefabTools.cpp
@@ -342,7 +342,8 @@ namespace {
 	}
 
 	bool ShouldSkipSerialization(const FProperty* Property, UObject* ObjToSerialize, APrefabActor* PrefabActor) {
-		if (const FObjectProperty* ObjProperty = CastField<FObjectProperty>(Property)) {
+		if (Property->StaticClassCastFlags() == CASTCLASS_FObjectProperty) {
+			const FObjectProperty* ObjProperty = (const FObjectProperty*)Property;
 			UObject* PropertyObjectValue = ObjProperty->GetObjectPropertyValue_InContainer(ObjToSerialize);
 			if (ContainsOuterParent(PropertyObjectValue, ObjToSerialize) ||
 				ContainsOuterParent(PropertyObjectValue, PrefabActor)) {
@@ -407,14 +408,6 @@ namespace {
 				continue;
 			}
 
-			if (const FObjectProperty* ObjProperty = CastField<FObjectProperty>(Property)) {
-				UObject* PropertyObjectValue = ObjProperty->GetObjectPropertyValue_InContainer(ObjToSerialize);
-				if (PropertyObjectValue && PropertyObjectValue->HasAnyFlags(RF_DefaultSubObject | RF_ArchetypeObject)) {
-					continue;
-				}
-			}
-
-
 			PropertiesToSerialize.Add(Property);
 		}
 
@@ -438,13 +431,10 @@ namespace {
 			// Check for cross actor references
 			bool bFoundCrossReference = false;
 
-			if (const FObjectProperty* ObjProperty = CastField<FObjectProperty>(Property)) {
+			if (Property->StaticClassCastFlags() == CASTCLASS_FObjectProperty) {
+				const FObjectProperty* ObjProperty = (const FObjectProperty*)Property;
 				UObject* PropertyObjectValue = ObjProperty->GetObjectPropertyValue_InContainer(ObjToSerialize);
 				if (PropertyObjectValue) {
-					if (PropertyObjectValue->HasAnyFlags(RF_DefaultSubObject | RF_ArchetypeObject)) {
-						continue;
-					}
-
 					FString ObjectPath = PropertyObjectValue->GetPathName();
 					FGuid CrossRefPrefabItem;
 					if (CrossReferences.GetPrefabItemId(ObjectPath, CrossRefPrefabItem)) {
@@ -646,14 +636,14 @@ void FPrefabTools::LoadActorState(AActor* InActor, const FPrefabricatorActorData
 		}
 	}
 
-	InActor->PostLoad();
-	InActor->ReregisterAllComponents();
-
 #if WITH_EDITOR
 	if (InActorData.ActorName.Len() > 0) {
 		InActor->SetActorLabel(InActorData.ActorName);
 	}
 #endif // WITH_EDITOR
+
+	InActor->PostLoad();
+	InActor->ReregisterAllComponents();
 
 	if (Service.IsValid()) {
 		SCOPE_CYCLE_COUNTER(STAT_LoadActorState_EndTransaction);
@@ -912,8 +902,9 @@ void FPrefabTools::FixupCrossReferences(const TArray<UPrefabricatorProperty*>& P
 
 		FProperty* Property = ObjToWrite->GetClass()->FindPropertyByName(*PrefabProperty->PropertyName);
 
-		const FObjectProperty* ObjectProperty = CastField<FObjectProperty>(Property);
-		if (!ObjectProperty) continue;
+		if (Property->StaticClassCastFlags() != CASTCLASS_FObjectProperty) continue;
+
+		const FObjectProperty* ObjectProperty = (const FObjectProperty*)Property;
 
 		AActor** SearchResult = PrefabItemToActorMap.Find(PrefabProperty->CrossReferencePrefabActorId);
 		if (!SearchResult) continue;
