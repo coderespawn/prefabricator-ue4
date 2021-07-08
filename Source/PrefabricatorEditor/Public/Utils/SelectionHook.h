e2eb4ead16291076b69e1e8fbe7b25cd09834794
Ali Akbar <ali.akbar@coderespawn.com>
2021-07-08 12:14:02 +0530

    UE5 version was incorrectly selecting the inner actors when the prefab was selected.  This caused usability issues while moving / duplicating prefabs

--- Source/PrefabricatorEditor/Public/Utils/SelectionHook.h
+++ Source/PrefabricatorEditor/Public/Utils/SelectionHook.h
@@ -4,10 +4,21 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 
-class PREFABRICATOREDITOR_API FPrefabricatorSelectionHook {
+struct FPrefabricatorSelectionRequest {
+	TWeakObjectPtr<AActor> Actor;
+	bool bSelected;
+};
+
+class PREFABRICATOREDITOR_API FPrefabricatorSelectionHook : public FTickableEditorObject {
 public:
 	void Initialize();
 	void Release();
+
+	//~ Begin FTickableEditorObject Interface
+	virtual void Tick(float DeltaTime) override;
+	virtual bool IsTickable() const override;
+	virtual TStatId GetStatId() const override;
+	//~ End FTickableEditorObject Interface
 
 private:
 	void OnObjectSelected(UObject* Object);
@@ -18,5 +29,6 @@ private:
 	FDelegateHandle CallbackHandle_SelectNone;
 	TWeakObjectPtr<AActor> LastSelectedObject;
 	bool bSelectionGuard = false;
+	TQueue<FPrefabricatorSelectionRequest> SelectionRequests;
 };
 
