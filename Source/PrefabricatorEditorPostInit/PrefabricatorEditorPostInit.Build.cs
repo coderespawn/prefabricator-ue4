68b3f422095d480d4878d077a70754e1cb2442da
Ali Akbar <ali.akbar@coderespawn.com>
2021-09-09 14:25:44 +0530

    Enabled unity build in engine specific branch

--- Source/PrefabricatorEditorPostInit/PrefabricatorEditorPostInit.Build.cs
+++ Source/PrefabricatorEditorPostInit/PrefabricatorEditorPostInit.Build.cs
@@ -6,7 +6,6 @@ namespace UnrealBuildTool.Rules
 	{
 		public PrefabricatorEditorPostInit(ReadOnlyTargetRules Target) : base(Target)
         {
-            bUseUnity = false;
             PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
             PublicIncludePaths.AddRange(
 				new string[] {
