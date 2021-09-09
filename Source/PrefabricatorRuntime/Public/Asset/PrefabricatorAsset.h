fc47c75c98afb3ed0aa1a5e6fdc84c576ac757f4
Ali Akbar <ali.akbar@coderespawn.com>
2021-09-09 14:12:08 +0530

    Bumped up the version to 1.7.0.  Updated the changelog?

--- Source/PrefabricatorRuntime/Public/Asset/PrefabricatorAsset.h
+++ Source/PrefabricatorRuntime/Public/Asset/PrefabricatorAsset.h
@@ -103,7 +103,7 @@ public:
 	virtual void PostSpawn_Implementation(APrefabActor* Prefab);
 };
 
-UCLASS(Blueprintable)
+UCLASS(Abstract, BlueprintType, Blueprintable)
 class PREFABRICATORRUNTIME_API UPrefabricatorAssetInterface : public UObject {
 	GENERATED_BODY()
 public:
