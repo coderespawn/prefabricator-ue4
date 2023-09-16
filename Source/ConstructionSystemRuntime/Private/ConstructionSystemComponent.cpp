92a61a33b2b6f7ad77354bfcd53414496879db99
Ali Akbar <ali.akbar@coderespawn.com>
2023-09-17 00:40:27 +0530

    Updated the scirpts.  Updated the copyright year to 2o23

--- Source/ConstructionSystemRuntime/Private/ConstructionSystemComponent.cpp
+++ Source/ConstructionSystemRuntime/Private/ConstructionSystemComponent.cpp
@@ -1,4 +1,4 @@
-//$ Copyright 2015-22, Code Respawn Technologies Pvt Ltd - All Rights Reserved $//
+//$ Copyright 2015-23, Code Respawn Technologies Pvt Ltd - All Rights Reserved $//
 
 #include "ConstructionSystemComponent.h"
 
